1a29f3031cd34c451f4137dbe36800807f2ece54
Heloisa Reis <helosilvareis@hotmail.com>
2017-10-30 16:12:48 -0200

    Adding Code

---
+++ EP.CursoMVC.Domain/Models/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EP.CursoMVC.Domain.Models
+{
+    public class Cliente
+    {
+        public Cliente()
+        {
+            ID = Guid.NewGuid();
+        }
+        public Guid ID { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string CPF { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+        public bool Excluido { get; set; }
+        public ICollection<Endereco> Enderecos { get; set; }
+    }
+}
---
+++ EP.CursoMVC.Domain/Models/Endereco.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EP.CursoMVC.Domain.Models
+{
+    public class Endereco
+    {
+        public Endereco()
+        {
+            ID = Guid.NewGuid();
+        }
+        public Guid ID { get; set; }
+
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string CEP { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public Guid ClienteId { get; set; }
+
+        //Propriedade de navegação
+        public Cliente Cliente { get; set; }
+    }
+}
